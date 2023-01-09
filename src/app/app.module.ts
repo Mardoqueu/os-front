248735a517acd552e80e211fe88c14842fa6baea
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-09 13:59:41 -0300

    adicionando todos os importes do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http'; 
+import { MatSelectModule } from '@angular/material/select'; 
+import { MatInputModule } from '@angular/material/input'; 
+import { MatDatepickerModule } from '@angular/material/datepicker'; 
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatListModule } from '@angular/material/list'; 
+import { MatCardModule } from '@angular/material/card'; 
+import { MatTableModule } from '@angular/material/table'; 
+import { MatPaginatorModule } from '@angular/material/paginator'; 
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { HeaderComponent } from './views/components/template/header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserModule, 
+    AppRoutingModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    BrowserAnimationsModule, 
+    HttpClientModule, 
+    BrowserModule, 
+    AppRoutingModule, 
+    BrowserAnimationsModule, 
+    MatToolbarModule, 
+    MatSidenavModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    MatListModule, 
+    MatCardModule, MatTableModule, 
+    MatSelectModule, 
+    MatInputModule, 
+    MatDatepickerModule, 
+    MatPaginatorModule, 
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
