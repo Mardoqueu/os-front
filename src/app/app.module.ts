cb66eacaca28601b8b79ee654d51559e553560f9
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-12 17:47:07 -0300

    adding footercomponent into module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,12 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator'; 
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { HeaderComponent } from './views/components/template/header/header.component';
+import { FooterComponent } from './views/components/template/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule, 
